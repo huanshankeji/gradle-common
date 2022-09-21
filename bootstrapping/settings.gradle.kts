432e20771a8b594a15cc4498636c1b1f1f0a43e6
Shreck Ye <ShreckYe@gmail.com>
2022-09-21 23:04:45 +0800

    Rename the project and delete some more unneeded files

---
+++ bootstrapping/settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "bootstrapping-gradle-plugins"
+include("kotlin-common-gradle-plugins-bootstrapping")
+include("huanshankeji-team-gradle-plugins-bootstrapping")
--- common-bootstrapping-gradle-plugins/settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name = "common-bootstrapping-gradle-plugins"
-include("plugin")
