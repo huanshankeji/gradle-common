1e2a02a205886df14d39d4cc98d531b8b4c050b7
Shreck Ye <ShreckYe@gmail.com>
2022-09-24 05:49:00 +0800

    Restore a version number that was changed during debugging, which shouldn't have been committed

--- common-gradle-dependencies/build.gradle.kts
+++ common-gradle-dependencies/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 // "x.y.z" indicates the version of the way of organizing the code,
 // and the date indicates the version when the dependency versions are updated.
-version = "0.3.2-20220728-test"
+version = "0.3.2-20220728-SNAPSHOT"
 
 generateKotlinVersion(kotlinVersion)
 
