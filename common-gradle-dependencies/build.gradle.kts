f707661a862b2a6e3bf2e421b50def8f43558c58
Shreck Ye <ShreckYe@gmail.com>
2022-09-24 02:09:56 +0800

    Resolve the issue that a SNAPSHOT version of "common-gradle-dependencies" is published to the OSSRH release repository as mentioned in the previous commit with a temporary workaround helper plugin
    This reveals a potential bug in the `sonatype-ossrh-publish` plugin that the repository url can be set before the actual project version is set.

--- common-gradle-dependencies/build.gradle.kts
+++ common-gradle-dependencies/build.gradle.kts
@@ -3,17 +3,13 @@ import com.huanshankeji.team.`Shreck Ye`
 import com.huanshankeji.team.pomForTeamDefaultOpenSource
 
 plugins {
-    `build-dependency-library-conventions`
+    `common-gradle-dependencies-helper`
 
     // This project is published to Maven Central instead of the Gradle Plugin Portal.
 
     `java-gradle-plugin` // This plugin is needed to add the necessary Gradle dependencies.
     id("com.huanshankeji.sonatype-ossrh-publish")
 }
-
-// "x.y.z" indicates the version of the way of organizing the code,
-// and the date indicates the version when the dependency versions are updated.
-version = "0.3.2-20220728-SNAPSHOT"
 
 generateKotlinVersion(kotlinVersion)
 
