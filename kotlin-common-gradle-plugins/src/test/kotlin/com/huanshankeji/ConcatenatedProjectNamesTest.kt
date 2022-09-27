9b1e9f3e310ced54c61687bb289b0dfd02978bfd
Shreck Ye <ShreckYe@gmail.com>
2022-09-27 20:56:48 +0800

    Improve the test in the previous commit

--- kotlin-common-gradle-plugins/src/test/kotlin/com/huanshankeji/ConcatenatedProjectNamesTest.kt
+++ kotlin-common-gradle-plugins/src/test/kotlin/com/huanshankeji/ConcatenatedProjectNamesTest.kt
@@ -6,8 +6,13 @@ import kotlin.test.assertEquals
 class ConcatenatedProjectNamesTest {
     @Test
     fun `test getConcatenatedProjectNamePath`() {
-        assertEquals("a", getConcatenatedProjectNamePath("a"))
-        assertEquals("a:a-b", getConcatenatedProjectNamePath("a:b"))
-        assertEquals("a:a-b:a-b-c", getConcatenatedProjectNamePath("a:b:c"))
+        fun testWithAndWithoutLeadingColon(expected: String, path: String) {
+            assertEquals(expected, getConcatenatedProjectNamePath(path))
+            assertEquals(":$expected", ":${getConcatenatedProjectNamePath(path)}")
+        }
+
+        testWithAndWithoutLeadingColon("a", "a")
+        testWithAndWithoutLeadingColon("a:a-b", "a:b")
+        testWithAndWithoutLeadingColon("a:a-b:a-b-c", "a:b:c")
     }
 }
