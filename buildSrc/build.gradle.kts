huanshankeji/gradle-common

commit 4557ca671ad2b3cac2803ac4186bdef697a6473e

Add "com.huanshankeji:common-gradle-dependencies:0.3.2-20220728" as a bootstrapping dependency in build dependencies and improve `com.huanshankeji.sonatype-ossrh-publish`

buildSrc/build.gradle.kts
@@ -19,4 +19,6 @@ dependencies {
     implementation("com.gradle.publish:plugin-publish-plugin:1.0.0-rc-2")
     // This is a bootstrapping dependency (cross-version self-dependency). Try not to update its version unless necessary.
     implementation("com.huanshankeji.team:gradle-plugins:0.3.0")
+    // This is also a bootstrapping dependency.
+    implementation("com.huanshankeji:common-gradle-dependencies:0.3.2-20220728")
 }

buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -1,0 +1,7 @@
+import com.huanshankeji.CommonGradleClasspathDependencies
+import com.huanshankeji.CommonVersions
+
+val commonVersions = CommonVersions()
+val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
+
+val kotlinVersion = commonVersions.kotlin

architecture-common-gradle-plugins/build.gradle.kts
@@ -16,7 +16,9 @@ dependencies {
      enabling it to always depend on a release version. */
     implementation("com.huanshankeji:common-gradle-dependencies:0.3.1-20220728")
 
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.2.0-alpha01-dev753") // TODO: extract "common-gradle-dependencies" into a standalone project and use the dependency there
+    with(commonGradleClasspathDependencies.composeJb) {
+        implementation("$gradlePluginProjectGroupAndArtifact:$defaultVersion")
+    }
 }
 
 gradlePlugin {

buildSrc/src/main/kotlin/KotlinVersion.kt
@@ -1,1 +1,0 @@
-const val kotlinVersion = "1.7.10"

buildSrc/src/main/kotlin/aligned-version-plugin-conventions.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("conventions")
 }
 
-version = "0.3.1-SNAPSHOT"
+version = "0.3.2-SNAPSHOT"

kotlin-common-gradle-plugins/src/main/kotlin/com/huanshankeji/java-1-8-compatibility-publish-conventions.gradle.kts
@@ -1,7 +1,5 @@
 package com.huanshankeji
 
-import gradle.kotlin.dsl.accessors._ecdd123130069d2da9a9acddf01898e4.java
-
 plugins {
     java
 }

kotlin-common-gradle-plugins/src/main/kotlin/com/huanshankeji/sonatype-ossrh-publish.gradle.kts
@@ -10,13 +10,16 @@ signing {
     sign(publishing.publications)
 }
 
-publishing.repositories.maven {
-    name = "SonatypeOssrh"
-    val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-    val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-    credentials {
-        project.findProperty("ossrhUsername")?.let { username = it as String }
-        project.findProperty("ossrhPassword")?.let { password = it as String }
+// wrapped in `afterEvaluate` because the project version may be set after applying this plugin, on which whether to choose the release URL or the snapshot URL depends
+afterEvaluate {
+    publishing.repositories.maven {
+        name = "SonatypeOssrh"
+        val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+        val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+        url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+        credentials {
+            project.findProperty("ossrhUsername")?.let { username = it as String }
+            project.findProperty("ossrhPassword")?.let { password = it as String }
+        }
     }
 }

commit 98e9bc9dec8c7dd45ba9853fd70b0c7fb2d8ec13

Bump Compose to 1.2.1, Kotlin to 1.7.21, and the Gradle publish plugin to 1.1.0, and fix a cyclic dependency `kotlinVersion` issue
`kotlinVersion` in "buildSrc/src/main/kotlin/VersionsAndDependencies.kt" and `CommonVersions.kotlin` in "common-gradle-dependencies/src/main/kotlin/com/huanshankeji/CommonVersions.kt" depend on each other, making it impossible to bump the Kotlin version. This issue was introduced by commit 4557ca671ad2b3cac2803ac4186bdef697a6473e.

buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
 }
 
 repositories {
@@ -17,7 +17,7 @@ repositories {
 
 dependencies {
     implementation("org.gradle.kotlin:gradle-kotlin-dsl-plugins:2.3.3") // It seems this version has to be used for Gradle 7.5.
-    implementation("com.gradle.publish:plugin-publish-plugin:1.0.0-rc-2")
+    implementation("com.gradle.publish:plugin-publish-plugin:1.1.0")
     // This is a bootstrapping dependency (cross-version self-dependency). Try not to update its version unless necessary.
     implementation("com.huanshankeji.team:gradle-plugins:0.3.0")
     // This is also a bootstrapping dependency.

buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -4,4 +4,4 @@ import com.huanshankeji.CommonVersions
 val commonVersions = CommonVersions()
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
 
-val kotlinVersion = commonVersions.kotlin
+val kotlinVersion = "1.7.21"

common-gradle-dependencies/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 // "x.y.z" indicates the version of the way of organizing the code,
 // and the date indicates the version when the dependency versions are updated.
-version = "0.3.3-20220728-SNAPSHOT"
+version = "0.3.3-20221120-SNAPSHOT"
 
 generateKotlinVersion(kotlinVersion)
 

common-gradle-dependencies/src/main/kotlin/com/huanshankeji/CommonVersions.kt
@@ -13,7 +13,7 @@ class CommonVersions(
     val kotlinxDatetime: String = "0.4.0",
     val exposed: String = "0.38.2",
     val ktor: String = "2.0.3",
-    val composeJb: String = "1.2.0-alpha01-dev753", // TODO: try to use a stable version in production
+    val composeJb: String = "1.2.1",
 
     val vertx: String = "4.3.2",
     val arrow: String = "1.1.2",