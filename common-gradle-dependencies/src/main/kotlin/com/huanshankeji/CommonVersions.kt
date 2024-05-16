679f3bc308199eeeafd71bd900d724275f643a79
Shreck Ye <ShreckYe@gmail.com>
2024-05-16 22:06:01 +0800

    Update the common dependency versions in `CommonVersions`

--- common-gradle-dependencies/src/main/kotlin/com/huanshankeji/CommonVersions.kt
+++ common-gradle-dependencies/src/main/kotlin/com/huanshankeji/CommonVersions.kt
@@ -7,20 +7,20 @@ class CommonVersions @JvmOverloads constructor(
 
     val kotlinCommon: String = "0.4.0",
 
-    val kotlinxCoroutines: String = "1.8.0",
+    val kotlinxCoroutines: String = "1.8.1",
     val kotlinxHtml: String = "0.11.0",
     val kotlinxSerialization: String = "1.6.3",
-    val kotlinxDatetime: String = "0.5.0",
+    val kotlinxDatetime: String = "0.6.0",
     val kotlinxBenchmark: String = "0.4.10",
-    val exposed: String = "0.48.0",
-    val ktor: String = "2.3.9",
+    val exposed: String = "0.50.1",
+    val ktor: String = "2.3.11",
     val composeMultiplatform: String = "1.6.2", // this is usually only used in classpath dependencies
 
-    val vertx: String = "4.5.5",
-    val arrow: String = "1.2.3",
-    val orgJunit: String = "5.10.2",
-    val kotest: String = "5.8.1",
-    val postgreSql: String = "42.7.2",
+    val vertx: String = "4.5.7",
+    val arrow: String = "1.2.4",
+    val orgJunit: String = "5.10.2", // JUnit 5 actually
+    val kotest: String = "5.9.0",
+    val postgreSql: String = "42.7.3",
     val slf4j: String = "1.7.36", // TODO: consider replacing with kotlin-logging (https://github.com/oshai/kotlin-logging)
-    val testContainers: String = "1.19.7"
+    val testContainers: String = "1.19.8"
 )
