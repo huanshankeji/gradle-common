cbd93560e8333f3ef875d166592c2bd2058d9f6a
Shreck Ye <ShreckYe@gmail.com>
2024-06-05 21:38:01 +0800

    Update `GenerateKotlinJsBrowserWebrootForVertxWebPlugin` to work with Kotlin 2.0.0

--- architecture-common-gradle-plugins/src/main/kotlin/com/huanshankeji/GenerateKotlinJsBrowserWebrootForVertxWebPlugin.kt
+++ architecture-common-gradle-plugins/src/main/kotlin/com/huanshankeji/GenerateKotlinJsBrowserWebrootForVertxWebPlugin.kt
@@ -2,50 +2,57 @@ package com.huanshankeji
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.util.PatternFilterable
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
 class GenerateKotlinJsBrowserWebrootForVertxWebPlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
         val extension = extensions.create<Extension>("generateKotlinJsResources")
 
+        afterEvaluate {
+            val frontendProject = project(extension.webFrontendProjectPath.get())
+            val jsBrowserDistributionTask = frontendProject.tasks.named("jsBrowserDistribution")
+            /*val jsBrowserWebpack by lazy {
+                tasks.getByPath(
+                    extension.webFrontendProjectPath.get() +
+                            if (extension.production.get()) ":jsBrowserProductionWebpack" else ":jsBrowserDevelopmentWebpack"
+                ) as KotlinWebpack
+            }*/
+            val copyJsBrowserDistributionToResourcesWebroot = "copyJsBrowserDistributionToResourcesWebroot"
+            val browserDistributionResourcesDirectory = layout.buildDirectory.get().dir("browserDistributionResources")
 
-        // see: https://play.kotlinlang.org/hands-on/Full%20Stack%20Web%20App%20with%20Kotlin%20Multiplatform/04_Frontend_Setup
+            tasks.register<Copy>(copyJsBrowserDistributionToResourcesWebroot) {
+                dependsOn(jsBrowserDistributionTask)
+                // TODO I didn't find a way to get this path using the DSL, so there may be a bug if this path is customized.
+                from(frontendProject.layout.buildDirectory.get().dir("dist/js/productionExecutable"))
+                //if (extension.production.get())
+                extension.includes.getOrNull()?.let { include(it) }
+                into(browserDistributionResourcesDirectory.dir("webroot"))
+            }
 
-        val jsBrowserDistributionTask by lazy {
-            tasks.getByPath(extension.webFrontendProjectPath.get() + ":jsBrowserDistribution")
+            tasks.named<Copy>("processResources") {
+                dependsOn(copyJsBrowserDistributionToResourcesWebroot)
+            }
+
+            sourceSets.main { resources.srcDir(browserDistributionResourcesDirectory) }
         }
-        val jsBrowserWebpack by lazy {
-            tasks.getByPath(
-                extension.webFrontendProjectPath.get() +
-                        if (extension.production.get()) ":jsBrowserProductionWebpack" else ":jsBrowserDevelopmentWebpack"
-            ) as KotlinWebpack
-        }
-        val copyJsBrowserDistributionToResourcesWebroot = "copyJsBrowserDistributionToResourcesWebroot"
-        val browserDistributionResourcesDirectory = buildDir.resolve("browserDistributionResources")
-
-        tasks.register<Copy>(copyJsBrowserDistributionToResourcesWebroot) {
-            dependsOn(jsBrowserWebpack)
-            dependsOn(jsBrowserDistributionTask) // needed for Gradle 8.0.1 whenever `production` is `true` or `false` // TODO: this may be a bug.
-            from(jsBrowserWebpack.destinationDirectory)
-            if (extension.production.get())
-                include("*.html", "*.css", "*.js")
-            into(browserDistributionResourcesDirectory.resolve("webroot"))
-        }
-
-        tasks.named<Copy>("processResources") {
-            dependsOn(copyJsBrowserDistributionToResourcesWebroot)
-        }
-
-        sourceSets.main { resources.srcDir(browserDistributionResourcesDirectory) }
     }
 
     interface Extension {
         val webFrontendProjectPath: Property<String>
-        val production: Property<Boolean>
+
+        //val production: Property<Boolean>
+        /**
+         * Patterns of distribution files to include.
+         * Defaults to including all files.
+         *
+         * @see PatternFilterable.include
+         */
+        val includes: ListProperty<String>
     }
 }
