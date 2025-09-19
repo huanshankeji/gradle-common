9834bfc989eaa754c024b7cfc4688910c14796c1
Yongshun Ye <ShreckYe@gmail.com>
2025-09-19 16:04:57 +0800

    Remove applying the `java-1-8-compatibility-publish-conventions` plugin to resolve an error
    ```text
    Inconsistent JVM Target Compatibility Between Java and Kotlin Tasks
    Inconsistent JVM-target compatibility detected for tasks 'compileJava' (1.8) and 'compileKotlin' (17).
    This will become an error in Gradle 8.0.
    Solution: Consider using JVM Toolchain: https://kotl.in/gradle/jvm/toolchain
    Learn more about JVM-target validation: https://kotl.in/gradle/jvm/target-validation
    ```

--- buildSrc/src/main/kotlin/conventions.gradle.kts
+++ buildSrc/src/main/kotlin/conventions.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("org.gradle.kotlin.kotlin-dsl")
     id("com.huanshankeji.team.with-group")
 
-    id("com.huanshankeji.java-1-8-compatibility-publish-conventions")
     id("com.gradle.plugin-publish")
     id("com.huanshankeji.team.default-github-packages-maven-publish")
 }
