0cdb29c45911deb3ada0edce9f92a2a71d7d1c50
Yongshun Ye <ShreckYe@gmail.com>
2025-09-29 15:17:20 +0800

    Revert "Try bumping Compose to v1.10.0-alpha01 to resolve some issues"
    This partially reverts commit 8e012784db982590997226271f860cb62f408d45.

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -7,7 +7,7 @@ val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(common
 
 object DependencyVersions {
     val kotlin = "2.2.20" // compatible with the compose version below
-    val composeMultiplatform = "1.10.0-alpha01"
+    val composeMultiplatform = "1.9.0"
     val kotlinxBenchmark = "0.4.14"
     val dokka = "2.1.0-Beta"
     val binaryCompatibilityValidator = "0.18.1"
