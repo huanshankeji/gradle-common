d151bc1959d10916f49a929c02c2da16818483ff
Yongshun Ye <ShreckYe@gmail.com>
2025-10-15 11:28:51 +0800

    Update the `jvm-integration-test` plugin with the latest code from https://docs.gradle.org/current/userguide/java_testing.html#sec:configuring_java_integration_tests

--- kotlin-common-gradle-plugins/src/main/kotlin/com/huanshankeji/jvm-integration-test.gradle.kts
+++ kotlin-common-gradle-plugins/src/main/kotlin/com/huanshankeji/jvm-integration-test.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 // code copied and adapted from https://docs.gradle.org/current/userguide/java_testing.html#sec:configuring_java_integration_tests
 
+
 sourceSets {
     create("intTest") {
         compileClasspath += sourceSets.main.get().output
@@ -15,12 +16,21 @@ sourceSets {
 
 val intTestImplementation by configurations.getting {
     extendsFrom(configurations.implementation.get())
-    extendsFrom(configurations.testImplementation.get())
 }
+val intTestRuntimeOnly by configurations.getting
 
 configurations["intTestRuntimeOnly"].extendsFrom(configurations.runtimeOnly.get())
 
-val integrationTest = tasks.register<Test>("integrationTest") {
+// commented out so the test library and version are not fixed
+/*
+dependencies {
+    intTestImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    intTestRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+*/
+
+
+val integrationTest = task<Test>("integrationTest") {
     description = "Runs integration tests."
     group = "verification"
 
@@ -28,8 +38,11 @@ val integrationTest = tasks.register<Test>("integrationTest") {
     classpath = sourceSets["intTest"].runtimeClasspath
     shouldRunAfter("test")
 
-    // copied from "kotlin-jvm-common-app-conventions"
     useJUnitPlatform()
+
+    testLogging {
+        events("passed")
+    }
 }
 
 tasks.check { dependsOn(integrationTest) }
