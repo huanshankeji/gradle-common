7a2f59cd32c02911caa40c443eb01997f845d2bc
Yongshun Ye <ShreckYe@gmail.com>
2026-01-26 02:52:13 +0800

    Bump all dependencies to the latest stable versions

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -10,10 +10,10 @@ object DependencyVersions {
     val kotlin = "2.3.0" // compatible with the compose version below
 
     // https://github.com/JetBrains/compose-multiplatform/releases
-    val composeMultiplatform = "1.9.1"
+    val composeMultiplatform = "1.10.0"
 
     // https://github.com/Kotlin/kotlinx-benchmark/releases
-    val kotlinxBenchmark = "0.4.14"
+    val kotlinxBenchmark = "0.4.15"
 
     // https://github.com/Kotlin/dokka/releases
     val dokka = "2.1.0"
@@ -22,14 +22,14 @@ object DependencyVersions {
     val binaryCompatibilityValidator = "0.18.1"
 
     // https://github.com/vanniktech/gradle-maven-publish-plugin/releases
-    val vanniktechMavenPublish = "0.34.0"
+    val vanniktechMavenPublish = "0.36.0"
 }
 
 val alignedPluginVersion = "0.11.0-SNAPSHOT"
 
 // "x.y.z" indicates the version of the way of organizing the code,
 // and the date indicates the version when the dependency versions are updated.
-val commonGradleDependenciesVersion = "0.10.0-20251224-SNAPSHOT"
+val commonGradleDependenciesVersion = "0.10.0-20260126-SNAPSHOT"
 
 // This is the source dependency version. There is another build source dependency in "buildSrc/build.gradle.kts".
 val pluginProjectSourceDependencyStableCommonGradleDependenciesVersion = "0.10.0-20251024".apply {
