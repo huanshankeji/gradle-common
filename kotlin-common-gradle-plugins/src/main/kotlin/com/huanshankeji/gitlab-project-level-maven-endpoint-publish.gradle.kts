573335233a832c0dcc1f93925fe221563b7093c9
Yongshun Ye <shreckye@gmail.com>
2026-04-19 13:05:53 +0800

    Add comments to clarify behavior for the GitLab and GitHub Maven publish plugins

--- kotlin-common-gradle-plugins/src/main/kotlin/com/huanshankeji/gitlab-project-level-maven-endpoint-publish.gradle.kts
+++ kotlin-common-gradle-plugins/src/main/kotlin/com/huanshankeji/gitlab-project-level-maven-endpoint-publish.gradle.kts
@@ -1,6 +1,12 @@
 package com.huanshankeji
 
 plugins {
+    /*
+    This plugin has special fast-path / custom behavior only for Maven Central,
+    while “other repositories” are delegated to plain Gradle maven-publish upload tasks.
+    Because of the slowness especially for multi-module Kotlin Multiplatform projects,
+    when running the task for multiple projects, run in parallel.
+     */
     id("com.vanniktech.maven.publish")
 }
 
