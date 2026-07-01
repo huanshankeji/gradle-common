3ce889d9e1021e11013304a65081828903ffcb54
Yongshun Ye <ShreckYe@gmail.com>
2026-07-01 14:23:57 +0800

    Rename and unify Gradle extension Kotlin code files

--- kotlin-common-gradle-plugins/src/main/kotlin/com/huanshankeji/publish/GradleGeneratedExtensions.kt
+++ kotlin-common-gradle-plugins/src/main/kotlin/com/huanshankeji/publish/GradleGeneratedExtensions.kt
@@ -4,5 +4,7 @@ import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 
+// copied and adapted from generated methods
+
 fun Project.publishing(configure: Action<PublishingExtension>): Unit =
     extensions.configure("publishing", configure)
