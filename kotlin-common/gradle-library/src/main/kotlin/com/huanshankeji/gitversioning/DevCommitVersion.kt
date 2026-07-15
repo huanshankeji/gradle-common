b51d5c4fbf95e61c55f6eae488608af17781ab5e
Yongshun Ye <shreckye@gmail.com>
2026-07-15 19:47:33 +0800

    Revert "Add non-experimental Project.projectVersionFromGitProvider convenience."
    This reverts commit c87c230254ba207fc906fbfbebd5ac6196e7d509.

--- kotlin-common/gradle-library/src/main/kotlin/com/huanshankeji/gitversioning/DevCommitVersion.kt
+++ kotlin-common/gradle-library/src/main/kotlin/com/huanshankeji/gitversioning/DevCommitVersion.kt
@@ -4,7 +4,6 @@ import com.huanshankeji.GradleCommonExperimentalApi
 import com.huanshankeji.git.gitCommitHash
 import com.huanshankeji.git.isGitWorkingTreeDirty
 import com.huanshankeji.git.workflow.isReleaseBranch
-import org.gradle.api.Project
 import org.gradle.api.provider.Provider
 import org.gradle.api.provider.ProviderFactory
 
@@ -42,11 +41,3 @@ fun ProviderFactory.projectVersionFromGitProvider(
         }
     }
 }
-
-/** Non-experimental [Project] convenience for [ProviderFactory.projectVersionFromGitProvider]. */
-@OptIn(GradleCommonExperimentalApi::class)
-fun Project.projectVersionFromGitProvider(
-    baseVersion: String,
-    releaseBranch: String = "release",
-): Provider<String> =
-    providers.projectVersionFromGitProvider(baseVersion, releaseBranch)
